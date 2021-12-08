d292c2e3a0ac1e7b4422c9ccd96cb97f008b5a13
Rahma Nader <rahmanaderm@gmail.com>
2021-12-08 18:14:09 +0200

    delete duplicate

--- Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        //ImageToPixels.writeImage(ImageToPixels.readImage("C:/Users/ahmge/Desktop/Semester 5/Data Compression/vector quantizer/image1.jpg"),510,340,"test1.jpg");
-        BlocksAndEuclidean.compress(4,4,0,"C:/Users/ahmge/Desktop/Semester 5/Data Compression/vector quantizer/image1.jpg");
-    }
-}
